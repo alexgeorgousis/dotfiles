bbe1dccaeeebe97fd1f344bf0bed71e817fbea13
Alex Georgousis <alex.georgousis@outlook.com>
2022-10-07 22:21:48 +0100

    Add autostart script

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,11 @@
+# Map CapsLock to Control
+setxkbmap -option ctrl:nocaps
+
+# Map short-pressed Control to Escape
+xcape -e 'Control_L=Escape'
+
+# Set faster key repeat and lower delay
+# Delay: 200ms
+# 45 keys per sec
+xset r rate 200 45
+
