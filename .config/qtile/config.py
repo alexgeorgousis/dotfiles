979c5224f694bdd3dc4bce289fa6ec142ae5a3d5
Alex Georgousis <alex.georgousis@outlook.com>
2022-10-08 12:22:40 +0100

    Qtile: Configure navigation keybinds

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -34,63 +34,69 @@ alt = "mod1"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     # Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
+
+    # Switch between screens
+    Key([alt], "h", lazy.prev_screen(), desc="Move focus to screen on the left"),
+    Key([alt], "l", lazy.next_screen(), desc="Move focus to screen on the right"),
+
+    # Move windows
     Key([alt, "control"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([alt, "control"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([alt, "control"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([alt, "control"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+
+    # Resize windows
     Key([alt, "shift"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([alt, "shift"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([alt, "shift"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([alt, "shift"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([alt, "shift"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
     Key(
-        [mod, "shift"],
+        [alt, "shift"],
         "Return",
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([alt, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([alt, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "space", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in "123"]
 
 for g in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
+            # alt + letter of group = switch to group
             Key(
                 [alt],
                 g.name,
                 lazy.group[g.name].toscreen(),
                 desc="Switch to group {}".format(g.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
+            # alt + shift + letter of group = switch to & move focused window to group
             Key(
                 [alt, "shift"],
                 g.name,
-                lazy.window.togroup(g.name, switch_group=True),
+                lazy.window.togroup(g.name),
                 desc="Switch to & move focused window to group {}".format(g.name),
             ),
             # Or, use below if you prefer not to switch to that group.
