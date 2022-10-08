d850f9fbbe282bf7b0a8e9ca77e8b18dd0417f0d
Alex Georgousis <alex.georgousis@outlook.com>
2022-10-08 09:46:49 +0100

    Add screen layout to autostart script

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -12,3 +12,5 @@ xset r rate 200 45
 # Start picom
 picom -b
 
+xrandr --output eDP --mode 3840x2160 --pos 4920x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 3840x2160 --pos 1080x0 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 0x120 --rotate right
+
