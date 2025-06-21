40da516d0a56204d07de46db0e5d8b95a1638def
Alex Georgousis <alex.georgousis@hey.com>
2025-06-21 12:15:06 +0100

    Set up home-manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.alex = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./.config/nixos/configuration.nix
         ./.config/nixos/hardware-configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.alex = import ./.config/home-manager/home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
       ];
     };
   };
