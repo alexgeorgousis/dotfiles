7ebe259f7aaf52ed06511f490f8cbf308ff5d68f
Alex Georgousis <alex.georgousis@hey.com>
2025-06-21 20:47:44 +0100

    nvim: restore s keybind

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,6 @@
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Map j-k to Escape" })
 vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Use H to move to beginning of the line" })
 vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Use L to move to end of the line" })
+
+-- Restore "s" in normal mode to replace a character - it's taken over by the plugin that lets you jump to places in the buffer (don't remember what it's called).
+vim.keymap.del("n", "s")
