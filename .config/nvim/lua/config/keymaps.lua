e80b10b57c6779176dd1567026b4554cfd826336
Alex Georgousis <alex.georgousis@hey.com>
2025-06-21 20:03:31 +0100

    nvim: set my standard keybinds

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Map j-k to Escape" })
+vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Use H to move to beginning of the line" })
+vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Use L to move to end of the line" })
