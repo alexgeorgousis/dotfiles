0b2c8b47a47536503256f3cff4a30957afa31c34
Alex Georgousis <alex.georgousis@hey.com>
2025-06-27 17:49:41 +0100

    Simplify jk keybind

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,7 @@ function fish_user_key_bindings
         # To see all modes run `bind --list-modes` in the terminal
         # backward-char - ensures the cursor moves 1 char back when switching to Normal mode
         # repaint - redraws the prompt to reprint the mode indication
-        bind --mode insert jk 'set fish_bind_mode default' backward-char repaint
+        bind --mode insert --sets-mode default jk backward-char repaint-mode
 
         bind --mode default \cg clear-screen "git status" repaint
         bind --mode insert \cg clear-screen "git status" repaint
