025cf5133a61170f4109cded4d0d18ec956a9ba6
Alex Georgousis <alex.georgousis@hey.com>
2025-06-27 16:50:27 +0100

    Add git status keybind

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -4,4 +4,8 @@ function fish_user_key_bindings
         # backward-char - ensures the cursor moves 1 char back when switching to Normal mode
         # repaint - redraws the prompt to reprint the mode indication
         bind --mode insert jk 'set fish_bind_mode default' backward-char repaint
+
+        bind --mode default \cg clear-screen "git status" repaint
+        bind --mode insert \cg clear-screen "git status" repaint
+        bind --mode visual \cg clear-screen "git status" repaint
 end
