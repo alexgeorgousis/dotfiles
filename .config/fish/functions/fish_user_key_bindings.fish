90fc202275f8766c85fe8e6f865726b124523d94
Alex Georgousis <alex.georgousis@hey.com>
2025-06-27 12:04:10 +0100

    Improve entering insert mode

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,7 @@
 function fish_user_key_bindings
         # Normal mode is called "default" in fish
         # To see all modes run `bind --list-modes` in the terminal
-        bind --mode insert jk 'set fish_bind_mode default'
+        # backward-char - ensures the cursor moves 1 char back when switching to Normal mode
+        # repaint - redraws the prompt to reprint the mode indication
+        bind --mode insert jk 'set fish_bind_mode default' backward-char repaint
 end
