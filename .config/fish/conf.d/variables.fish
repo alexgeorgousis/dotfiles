e405b5b0327148dec78d6ac9c1cb19b35493a981
Alex Georgousis <alex.georgousis@hey.com>
2025-06-27 13:26:46 +0100

    Add variables config

---
+++ .config/fish/conf.d/variables.fish
@@ -0,0 +1,15 @@
+###############
+# FISH CONFIG #
+###############
+
+# Set the time that fish waits after you type a key that's part of a sequence to see if the rest of that sequence is coming or not (example: jk to change from insert to normal mode).
+set fish_sequence_key_delay_ms 100
+
+
+#########################
+# ENVIRONMENT VARIABLES #
+#########################
+
+# NOTE: -g is --global and -x is --export - read more here: https://fishshell.com/docs/current/language.html#shell-variables
+
+set -gx EDITOR nvim
