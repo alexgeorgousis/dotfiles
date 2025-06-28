19ef1cf82ce21cacae1bf983be1ab0d15a315b99
Alex Georgousis <alex.georgousis@hey.com>
2025-06-28 07:52:38 +0100

    Increase key sequence delay
    So that longer motions like ciw register.

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -6,7 +6,7 @@
 set -g fish_key_bindings fish_vi_key_bindings
 
 # Set the time that fish waits after you type a key that's part of a sequence to see if the rest of that sequence is coming or not (example: jk to change from insert to normal mode).
-set fish_sequence_key_delay_ms 100
+set fish_sequence_key_delay_ms 200
 
 
 #########################
