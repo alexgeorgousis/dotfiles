3a02927a2fe4fab7330486a774bdb69b62d7e362
Alex Georgousis <alex.georgousis@hey.com>
2025-06-28 08:37:50 +0100

    Remove fish welcome message

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -8,6 +8,9 @@ set -g fish_key_bindings fish_vi_key_bindings
 # Set the time that fish waits after you type a key that's part of a sequence to see if the rest of that sequence is coming or not (example: jk to change from insert to normal mode).
 set fish_sequence_key_delay_ms 200
 
+# Remove the fish welcome message at the top of every new session
+set fish_greeting ""
+
 
 #########################
 # ENVIRONMENT VARIABLES #
