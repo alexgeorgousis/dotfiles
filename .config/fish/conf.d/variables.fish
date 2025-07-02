f9f051af4ba891e919836e967a134518f6c8e415
Alex Georgousis <alex.georgousis@hey.com>
2025-07-02 17:46:44 +0100

    Silence gem command when setting GEM_HOME

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -19,5 +19,5 @@ set fish_greeting ""
 # NOTE: -g is --global and -x is --export - read more here: https://fishshell.com/docs/current/language.html#shell-variables
 
 set -gx EDITOR nvim
-set -gx GEM_HOME $(gem env user_gemhome)
+set -gx GEM_HOME (gem env user_gemhome &> /dev/null)
 set -gx PATH ~/.asdf/shims $GEM_HOME/bin $PATH
