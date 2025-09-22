603a0fb5d3d029697cb68afd254af23fa5a50bb4
Alex Georgousis <alex.georgousis@hey.com>
2025-09-22 08:42:00 +0100

    Change Copilot accept keybind from Ctrl+J to Ctrl+Y

---
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+-- GitHub Copilot AI assistant
+-- https://github.com/github/copilot.vim
+
+return {
+	"github/copilot.vim",
+	event = "InsertEnter",
+	config = function()
+		-- Use Ctrl+Y to accept suggestions
+		vim.g.copilot_no_tab_map = true
+		vim.api.nvim_set_keymap("i", "<C-Y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+		-- Use Ctrl+] for next suggestion, Ctrl+[ for previous
+		vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-next)", {})
+		vim.api.nvim_set_keymap("i", "<C-[>", "<Plug>(copilot-previous)", {})
+
+		-- Dismiss suggestion with Ctrl+\
+		vim.api.nvim_set_keymap("i", "<C-\\>", "<Plug>(copilot-dismiss)", {})
+	end,
+}
